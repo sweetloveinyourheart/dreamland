9708d782984255a466684c12743e373d27e64646
Nguyen Lam HTDQ <sweetloveinyourheart@gmail.com>
2022-07-14 21:18:07 +0700

    fix cache error

--- server/src/stats/stats.module.ts
+++ server/src/stats/stats.module.ts
@@ -1,22 +1,11 @@
-import { CacheModule, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { StatsService } from './stats.service';
 import { StatsResolver } from './stats.resolver';
 import { RealEstateModule } from 'src/real-estate/real-estate.module';
 import { ProjectModule } from 'src/project/project.module';
-import { RedisClientOptions } from 'redis';
-import * as redisStore from 'cache-manager-redis-store'
-
 
 @Module({
   imports: [
-    CacheModule.registerAsync<RedisClientOptions>({
-      isGlobal: true,
-      useFactory: () => ({
-        store: redisStore,
-        host: 'redis',
-        port: 6379
-      })
-    }),
     RealEstateModule,
     ProjectModule
   ],
