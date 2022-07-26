55d86269f62c087caffc21ba1754dbac4a846180
Nguyen Lam HTDQ <sweetloveinyourheart@gmail.com>
2022-07-27 00:11:27 +0700

    fix export xlsx err

--- admin/src/constants/apollo.js
+++ admin/src/constants/apollo.js
@@ -1,5 +1,5 @@
 // export const GRAPHQL_ENDPOINT = "http://localhost:9000/graphql"
 // export const API_ENDPOINT = 'http://localhost:9000/api'
 export const GRAPHQL_ENDPOINT = "https://dienkhoiland.vn/graphql"
-export const API_ENDPOINT = 'http://dienkhoiland.vn/api'
+export const API_ENDPOINT = 'https://dienkhoiland.vn/api'
 
